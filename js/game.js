22cb7da751da85b0cc661de5f63a59557162ca00
Tom Dirinck <tom.dirinck@gmail.com>
2021-04-30 11:41:49 +0200

    Added wincheck, gamereset, correct turning of cards.

--- js/game.js
+++ js/game.js
@@ -24,8 +24,8 @@ const randomizeCards = () => {
 };
 
 const revealCard = (target) => {
-  target.classList.toggle('show');
-  target.classList.toggle('disabled');
+  target.classList.remove('hide');
+  target.classList.add('disabled');
 };
 
 const checkCards = (target) => {
@@ -38,7 +38,6 @@ const checkCards = (target) => {
     }
     clickedCards = [];
   }
-  console.log(clickedCards);
 };
 
 const match = () => {
@@ -48,13 +47,39 @@ const match = () => {
 };
 
 const unmatch = () => {
-  console.log();
+  clickedCards.forEach((card) => {
+    card.classList.add('hide');
+    card.classList.remove('disabled');
+  });
 };
+
+const checkWin = () => {
+  const matches = document.querySelectorAll('.card-back.match');
+  return matches.length === 16 ? true : false;
+};
+
+const ResetGame = () => {
+  if (checkWin()) startGame();
+};
+
+const startGame = () => {
+  document.querySelectorAll('.card-back').forEach((card) => {
+    card.classList.add('hide');
+    card.classList.remove('match');
+    card.classList.remove('disabled');
+  });
+  randomizeCards();
+};
+startGame();
 
 cards.forEach((card) => {
   card.addEventListener('click', (e) => {
     revealCard(e.target);
-    checkCards(e.target);
-    // console.log(e.target);
+    setTimeout(() => {
+      checkCards(e.target);
+    }, 500);
+    setTimeout(() => {
+      ResetGame();
+    }, 9500);
   });
 });
